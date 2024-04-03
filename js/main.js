dd10065501a122f203d710711019d5ae30b15e76
antoninlancesseur@gmail.com <antoninlancesseur@gmail.com>
2024-04-03 18:01:23 +0200

    gestion du clic en dehors du cercle

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
+const circleColor = getComputedStyle(document.documentElement).getPropertyValue('--circle');
+const circleErrorColor = getComputedStyle(document.documentElement).getPropertyValue('--circle-error');
+
 const placement = document.getElementById("placement");
 const aim = document.getElementById("aim");
 const startMenu = document.querySelector('.startMenu');
 const scoreMenu = document.querySelector('.scoreMenu');
 const scoreTxt = document.querySelector('.score');
+const zone = document.querySelector('.content');
 
 var score = 0;
+var failed = 0;
 var started = false;
 
 function start() {
     aim.setAttribute("onclick", 'touch()');
+    zone.setAttribute("onclick", "fail()");
     startMenu.style.display = 'none';
     scoreMenu.style.display = 'block';
     touch();
+    started = true;
 }
 
 function touch() {
@@ -23,4 +30,8 @@ function touch() {
     placement.style.top = posy + '%';
     placement.style.transform = `translate(-${posx}%, -${posy}%)`;
     scoreTxt.textContent = `Score : ${score}`;
+}
+
+function fail() {
+    failed++;
 }
